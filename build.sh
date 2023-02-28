8744a37d01cdf9e356d910711b14b445b8663a4d
jianxuesong <jianxuesong@sina.cn>
2023-03-01 01:25:15 +0800

    build

--- build.sh
+++ build.sh
@@ -1,41 +1,55 @@
 #!/bin/sh
 ###
- # @Author: Cedar
- # @Date: 2019-06-19 09:37:45
- # @LastEditors: Cedar
- # @LastEditTime: 2021-01-28 11:23:17
- # @FilePath: /Mango/build.sh
+ # @Description: Do not edit
+ # @Author: Jianxuesong
+ # @Date: 2021-05-31 21:53:23
+ # @LastEditors: Jianxuesong
+ # @LastEditTime: 2021-05-31 21:54:35
+ # @FilePath: /Melon/build.sh
 ### 
 #
 
 #当前版本号,每次更新服务时都必须更新版本号
 CurrentVersion=$(date +%m.%d.%H%M)
 
+# pwd
+
 export GOROOT=/usr/local/go
 export GOPATH=/usr/local/go/path
 export GOPROXY=https://goproxy.io
+# export GOCACHE=/tmp
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 
-Path=$(cd `dirname $0`; pwd)
-Build=$(basename `pwd` | tr 'A-Z' 'a-z')
+# go env
 
-ServerName=$(basename `pwd` | tr 'A-Z' 'a-z')
+go mod download
+go mod tidy -v
+
+Path=$(cd `dirname $0`; pwd)
+# Mod=$(head -n 1 go.mod)
+read _ Mod < go.mod # 读取一行，空格分隔，获得mod名字
+Build=$Mod
+# echo $Build
+
+ServerName=$(echo $Mod | awk -F/ '{print $3}' | tr 'A-Z' 'a-z') # 获取最后/分隔最后的名字 melon
+# echo $ServerName
+
+# ServerName=$(basename `pwd` | tr 'A-Z' 'a-z')
 BuildTime=$(date "+%Y-%m-%d %H:%M:%S")
 GitCommit=$(git rev-parse --short HEAD || echo unsupported)
 GoVersion=`go version`
 GoVersion=${GoVersion##*version }
 
 
-go build -ldflags "-X $Build.Version=$CurrentVersion \
-                   -X $Build.GitCommit=$GitCommit \
-                   -X '$Build.BuildTime=$BuildTime' \
-                   -X '$Build.GoVersion=$GoVersion' \
-                   -X $Build.RunEnv=debug " \
+go build -ldflags "-X main.AppVersion=$CurrentVersion \
+                   -X main.GitCommit=$GitCommit \
+                   -X 'main.BuildTime=$BuildTime' \
+                   -X 'main.GoVersion=$GoVersion' \
+                   -X main.RunEnv=release " \
         -o ./bin/$ServerName ./main.go
 
 
 if [ $? -eq 0 ]
 then
-    $Path/bin/$ServerName -v
+    $Path/bin/$ServerName -V
 fi
-
