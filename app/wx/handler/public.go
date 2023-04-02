56e59ed15a54515b9a980568f3537404a13e06b8
jianxuesong <jianxuesong@sina.cn>
2023-04-02 23:30:19 +0800

    123

--- app/wx/handler/public.go
+++ app/wx/handler/public.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	jsoniter "github.com/json-iterator/go"
@@ -47,20 +46,20 @@ func PublicMsg(c *gin.Context) {
 	// }
 
 	// fmt.Printf("%+v\n", chat)
-	replyContent := "I don't know"
+	// replyContent := "I don't know"
 	// if len(chat.Choices) > 0 {
 	// 	replyContent = chat.Choices[0].Text
 	// }
 
 	// 回复信息
-	reply, _ := xml.Marshal(define.ReplyText{
-		XMLName:      xml.Name{Local: "xml"},
-		ToUsername:   define.CDATA{Value: content.FromUsername},
-		FromUsername: define.CDATA{Value: content.ToUsername},
-		CreateTime:   time.Now().Unix(),
-		MsgType:      define.CDATA{Value: "text"},
-		Content:      define.CDATA{Value: replyContent},
-	})
+	// reply, _ := xml.Marshal(define.ReplyText{
+	// 	XMLName:      xml.Name{Local: "xml"},
+	// 	ToUsername:   define.CDATA{Value: content.FromUsername},
+	// 	FromUsername: define.CDATA{Value: content.ToUsername},
+	// 	CreateTime:   time.Now().Unix(),
+	// 	MsgType:      define.CDATA{Value: "text"},
+	// 	Content:      define.CDATA{Value: replyContent},
+	// })
 
 	// encryptMsg, cryptErr := wxcpt.EncryptMsg(string(reply), timestamp, nonce)
 	// if cryptErr != nil {
@@ -68,8 +67,8 @@ func PublicMsg(c *gin.Context) {
 	// 	return
 	// }
 
-	// reply := []byte("<xml><ToUserName><![CDATA[oDk236LGfpuPCzAqH09I9RzFYw1c]]></ToUserName><FromUserName><![CDATA[gh_2680178c02e1]]></FromUserName><CreateTime>1680448101</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好]]></Content></xml>")
-	reply = []byte("<xml><ToUsername><![CDATA[oDk236LGfpuPCzAqH09I9RzFYw1c]]></ToUsername><FromUsername><![CDATA[gh_2680178c02e1]]></FromUsername><CreateTime>1680449116</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好]]></Content></xml>")
+	reply := []byte("<xml><ToUserName><![CDATA[oDk236LGfpuPCzAqH09I9RzFYw1c]]></ToUserName><FromUserName><![CDATA[gh_2680178c02e1]]></FromUserName><CreateTime>1680448101</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好]]></Content></xml>")
+	// reply = []byte("<xml><ToUsername><![CDATA[oDk236LGfpuPCzAqH09I9RzFYw1c]]></ToUsername><FromUsername><![CDATA[gh_2680178c02e1]]></FromUsername><CreateTime>1680449116</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好]]></Content></xml>")
 	fmt.Println("reply encry", string(reply))
 	if num, err := c.Writer.Write(reply); err != nil {
 		fmt.Println("返回消息失败: ", err.Error())
